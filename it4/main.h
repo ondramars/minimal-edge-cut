a9f6cfca09adb44cf6b7c6197c69b966ce031664
Ondřej März <marzondr@fit.cvut.cz>
2023-04-17 15:28:39 +0200

    Master-Slave logic.

--- it4/main.h
+++ it4/main.h
@@ -1,4 +1,3 @@
-#include <vector>
 #include "/usr/local/opt/libomp/include/omp.h"
 #include <iostream>
 #include <iomanip>    // std::setw
@@ -6,17 +5,16 @@
 #include <climits>
 #include <ios>        // std::left
 
+#include "state.h"
+
 #define STATES_LIM 8
 
-/*      ========================================================================================================================       */
+#define SOURCE_MASTER 0
+#define TAG_WORK 1
+#define TAG_DONE 2
+#define TAG_KILL 3
 
-
-/*
- *      Hrana může být neobarvená, nebo je přiřazena do množiny vrcholů X, nebo Y.
- */
-enum Color { X, Y, NONE };
-
-/*      ========================================================================================================================       */
+/*      ============================================================================================       */
 
 
 /*
@@ -38,33 +36,8 @@ private:
 };
 
 
-/*      ========================================================================================================================       */
 
-
-/*
- *      Třída reprezentující vrchol, uchovávající informace o sousedech.
- *
- *      Poskytuje rozhraní umožňující přidat záznam o sousedovi a získat jejich seznam.
- */
-class State {
-  
-public:
-    State();
-    State(int min, int colored, std::vector<Color> nodes)
-            : min(min), colored(colored), nodes(nodes) { }
-    
-    int getMin() { return min; }
-    int getColored() { return colored; }
-    std::vector<Color> getNodes() { return nodes; }
-    
-private:
-    int min;
-    int colored;
-    std::vector<Color> nodes;
-    
-};
-
-/*      ========================================================================================================================       */
+/*      ============================================================================================       */
 
 /*
  *      Třída reprezentující graf, umožňující provádět výpočet pro problém minimálního hranového řezu hranově ohodnoceného grafu.
@@ -101,92 +74,142 @@ private:
     int                                 _global_min;
     std::vector<CNodeNeighbor>          _neighbors;
     std::vector<Color>                  _best_conf;
+    
+    int                                 buffer_size;
 };
-
-std::vector<State> CGraph::generateStates(std::vector<Color> & nodes) {
-    
-    std::vector<State>  states;
-    std::queue<State>   statesQueue;
-    
-    State s = State(0, 0, nodes);
-    
-    statesQueue.push(s);
-    
-    while (statesQueue.size() < STATES_LIM) {
-        
-        State currentState = statesQueue.front();
-        std::vector<Color> nodes = currentState.getNodes();
-        
-        // UKONČUJÍCÍ PODMÍNKY
-        if (currentState.getColored() == nodes.size()) {
-            checkCandidate(currentState.getMin(), nodes ); // máme nový rekord?
-            break;
-        }
-        if (minTooMuch(currentState.getMin()) || !lowerEstimate(currentState.getMin(), currentState.getColored(), nodes)) {
-            break;
-        }
-        
-        statesQueue.pop();
-     
-        // PŘIŘADÍME DALŠÍ UZEL DO X POKUD SE TAM VEJDE A ZKONTROLUJEME ZDA TO MÁ SMYSL
-        
-        if (canColorTo(X, nodes)) {
-            
-                nodes.at( currentState.getColored() ) = X;
-            
-                int Xmin = cutWeight(X, currentState.getColored(), nodes);
-            
-                statesQueue.push( State( currentState.getMin() + Xmin,
-                                         currentState.getColored() + 1,
-                                         nodes
-                                        )
-                                 );
-        
-                nodes.at( currentState.getColored() ) = NONE;
-        }
-
-        // PŘIŘADÍME DALŠÍ UZEL DO Y POKUD SE TAM VEJDE A ZKONTROLUJEME ZDA TO MÁ SMYSL
-
-        if (canColorTo(Y, nodes)) {
-            
-                nodes.at( currentState.getColored() ) = Y;
-                
-                int Ymin = cutWeight(Y, currentState.getColored(), nodes);
-                
-                statesQueue.push( State( currentState.getMin() + Ymin,
-                                         currentState.getColored() + 1,
-                                         nodes
-                                        )
-                                 );
-            
-                nodes.at( currentState.getColored() ) = NONE;
-        }
-    }
-    
-    while (!statesQueue.empty()) {
-        
-        states.emplace_back(std::move(statesQueue.front()));
-        statesQueue.pop();
-    }
-    
-    return states;
-}
 
 void CGraph::solve( std::vector<Color> & nodes, int thread_num ) {
     
-    std::vector<State> states;
+    int procNum, procCount;
+    MPI_Comm_rank(MPI_COMM_WORLD, &procNum);
+    MPI_Comm_size(MPI_COMM_WORLD, &procCount);
     
-    states = generateStates(nodes);
-    std::cout << "States generated: " << states.size() << std::endl;
+    // velikost nodes plus min plus colored
+    buffer_size = nodes.size() + 1 + 1;
     
-    #pragma omp parallel for schedule(dynamic) default(none) firstprivate(nodes) shared(states)
-    for (auto &state : states) {
-        
-        DFS( state.getMin(), state.getColored(), state.getNodes() );
+    if (procNum == SOURCE_MASTER) {
+        std::cout << "--> master #" << procNum << std::endl;
+        master(nodes, procCount);
+    } else {
+        std::cout << "--> slave #" << procNum << std::endl;
+        slave(procCount);
     }
+}
 
+void CGraph::master( std::vector<Color> & nodes, int procCount ) {
+    
+    int workerCount = procCount - 1;
+    
+    std::vector<State> states;
+    states = generateStates(nodes);
+    std::queue<State> queue;
+    
+    for (auto & elem: states) queue.push(std::move(elem));
+    states.clear();
+    
+    // Odešleme všem vláknům task
+    for (int i = 1; i <= workerCount && !queue.empty(); i++) {
+        
+        State task = queue.front();
+        queue.pop();
+        
+        std::vector<int> serialized = task.serializeTask();
+        
+        MPI_Send(serialized.data(), (int) serialized.size(), MPI_INT, i, TAG_DO_TASK, MPI_COMM_WORLD);
+    }
+    
+    // Dokud jsou tasky, úkolovat
+    while (workerCount > 0) {
+        
+        std::vector<int> buffer( buffer_size );
+        
+        MPI_Status status;
+        MPI_Recv(buffer.data(), buffer_size, MPI_INT, MPI_ANY_SOURCE, TAG_DONE, MPI_COMM_WORLD, &status);
+
+        if (buffer.at(0) < _global_min) {
+            
+            std::vector<Color> toNodes;
+            
+            for (int i = 2; i < buffer.size(); i++) {
+                
+                toNodes.push_back( (Color) buffer.at(i));
+            }
+            
+            checkCandidate(min, toNodes);
+        }
+
+        // Pokud není další práce, ukončíme slave vlákno
+        if (queue.empty()) {
+            
+            int nothing = 0;
+            MPI_Send(&nothing, 1, MPI_INT, status.MPI_SOURCE, TAG_KILL, MPI_COMM_WORLD);
+            
+            workerCount--;
+        }
+        // Jinak pošleme další práci
+        else {
+            
+            State task = queue.front();
+            queue.pop();
+            
+            std::vector<int> serialized = task.serializeTask(bestPrice);
+            MPI_Send(serialized.data(), (int) serialized.size(), MPI_INT, status.MPI_SOURCE, TAG_WORK, MPI_COMM_WORLD);
+        }
+    }
+    
     printResult();
 }
+
+void CGraph::slave ( int procCount ) {
+    
+    while (true) {
+        
+        std::vector<int> buffer( buffer_size );
+        
+        MPI_Status status;
+        MPI_Recv(buffer.data(), buffer_size, MPI_INT, SOURCE_MASTER, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+        if (status.MPI_TAG == TAG_KILL) {
+            break;
+        }
+        
+        if (status.MPI_TAG == TAG_DO_TASK) {
+            
+            State state = State::parseSerializedTask(buffer);
+            
+            /*
+             std::vector<State> states;
+             states = generateBoard(newState.chessboard, std::thread::hardware_concurrency() * 1000,newState.depth + 5, newState.depth);
+             
+             #pragma omp parallel for schedule(dynamic) default(none) shared(states)
+             for (auto &state : states) {
+             recursionSeq(state);
+             }
+             */
+            
+            // !!!!
+            
+            
+            
+            // !!!!
+            
+            // Volat DFS s parametrem stav a stav by asi měl mít uloženou i global_min?
+            
+            DFS( state.getMin(), state.getColored(), state.getNodes() );
+            
+            std::vector<int> serializedResult = state.serializeResult();
+            
+            MPI_Send(serializedResult.data(), (int) serializedResult.size(), MPI_INT, SOURCE_MASTER, TAG_TASK_DONE, MPI_COMM_WORLD);
+            
+        }
+    }
+}
+
+
+
+/*      ============================================================================================       */
+/*      MIN CUT LOGIC                                                                                      */
+/*      ============================================================================================       */
 
 int CGraph::cutWeight( Color color, int colored, std::vector<Color> & nodes ) {
     
@@ -263,16 +286,10 @@ bool CGraph::lowerEstimate(int min, int colored, std::vector<Color> & nodes) {
 void CGraph::checkCandidate(int min, std::vector<Color> & nodes) {
     
     if (min < _global_min) {
-        #pragma omp critical
-        {
-            if (min < _global_min) {
-                
-                _global_min = min;
-                _best_conf.clear();
-                for (Color col: nodes) {
-                    _best_conf.push_back( col );
-                }
-            }
+        _global_min = min;
+        _best_conf.clear();
+        for (Color col: nodes) {
+            _best_conf.push_back( col );
         }
     }
 }
@@ -315,7 +332,76 @@ void CGraph::DFS(int min, int colored, std::vector<Color> nodes) {
     }
 }
 
-/*      ========================================================================================================================       */
+std::vector<State> CGraph::generateStates(std::vector<Color> & nodes) {
+    
+    std::vector<State>  states;
+    std::queue<State>   statesQueue;
+    
+    State s = State(0, 0, nodes);
+    
+    statesQueue.push(s);
+    
+    while (statesQueue.size() < STATES_LIM) {
+        
+        State currentState = statesQueue.front();
+        std::vector<Color> nodes = currentState.getNodes();
+        
+        // UKONČUJÍCÍ PODMÍNKY
+        if (currentState.getColored() == nodes.size()) {
+            checkCandidate(currentState.getMin(), nodes ); // máme nový rekord?
+            break;
+        }
+        if (minTooMuch(currentState.getMin()) || !lowerEstimate(currentState.getMin(), currentState.getColored(), nodes)) {
+            break;
+        }
+        
+        statesQueue.pop();
+     
+        // PŘIŘADÍME DALŠÍ UZEL DO X POKUD SE TAM VEJDE A ZKONTROLUJEME ZDA TO MÁ SMYSL
+        
+        if (canColorTo(X, nodes)) {
+            
+                nodes.at( currentState.getColored() ) = X;
+            
+                int Xmin = cutWeight(X, currentState.getColored(), nodes);
+            
+                statesQueue.push( State( currentState.getMin() + Xmin,
+                                         currentState.getColored() + 1,
+                                         nodes
+                                        )
+                                 );
+        
+                nodes.at( currentState.getColored() ) = NONE;
+        }
+
+        // PŘIŘADÍME DALŠÍ UZEL DO Y POKUD SE TAM VEJDE A ZKONTROLUJEME ZDA TO MÁ SMYSL
+
+        if (canColorTo(Y, nodes)) {
+            
+                nodes.at( currentState.getColored() ) = Y;
+                
+                int Ymin = cutWeight(Y, currentState.getColored(), nodes);
+                
+                statesQueue.push( State( currentState.getMin() + Ymin,
+                                         currentState.getColored() + 1,
+                                         nodes
+                                        )
+                                 );
+            
+                nodes.at( currentState.getColored() ) = NONE;
+        }
+    }
+    
+    while (!statesQueue.empty()) {
+        
+        states.emplace_back(std::move(statesQueue.front()));
+        statesQueue.pop();
+    }
+    
+    return states;
+}
+
+/*      ============================================================================================       */
 
 /*
  *      Printing methods.
